8f18d942c2395e56b52f9d6bff30d3e3508bc9fe
Admin <kavyak6999@gmail.com>
2022-12-19 14:23:11 +0530

    function-1

---
+++ function/function.js
@@ -0,0 +1,16 @@
+var x = [1,2,3,4,5,6];
+alert (x);
+var z = x.length;
+alert (z);
+
+var x = [1,2,3,4,5,6];
+x.push(7);
+alert (x);
+var z = x.length;
+alert (z);
+
+function value(x,y){
+ return[x,y];
+}
+var z = value(6,5);
+console.log (z)
